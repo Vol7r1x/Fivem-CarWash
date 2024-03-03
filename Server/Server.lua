da04e019b7d3a920a85f66e8452b6c9f3db96e58
Vol7r1x <95163763+Vol7r1x@users.noreply.github.com>
2024-03-03 19:52:18 +0100

    Add files via upload

---
+++ Server/Server.lua
@@ -0,0 +1,27 @@
+ESX = exports["es_extended"]:getSharedObject()
+
+ESX.RegisterServerCallback('vol7r1x_CarWash:checkMoney',function(source, cb, wash)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local money = 0
+    local price = 0
+    if Config.PayCash then
+        money = xPlayer.getMoney()
+    else
+        money = xPlayer.getAccount('bank').money
+    end
+    if wash == "S" then
+        price = Config.PriceStandard
+    else
+        price = Config.PricePremium
+    end
+	if money >= price then
+		if Config.PayCash then
+			xPlayer.removeMoney(price)
+		else
+			xPlayer.removeAccountMoney('bank', price)
+		end
+        cb(true)
+    else
+        cb(false)
+    end
+end)
